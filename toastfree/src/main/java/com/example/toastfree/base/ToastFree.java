67a1b17391c24feeec6cb0bdec00f4341435bf48
邹 奇 <zouq@yanhuamedia.com>
2022-10-11 10:01:21 +0800

    新增图片吐丝api

--- toastfree/src/main/java/com/example/toastfree/base/ToastFree.java
+++ toastfree/src/main/java/com/example/toastfree/base/ToastFree.java
@@ -1,14 +1,25 @@
 package com.example.toastfree.base;
 
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.toastfree.R;
 
 public class ToastFree {
 
     private static ToastFree toastFree;
 
-    private Toast textToast;
+    private Toast toast_short,toast_long;
+
+    public static final int DURATION_SHORT = Toast.LENGTH_SHORT;
+    public static final int DURATION_LONG = Toast.LENGTH_LONG;
 
     private ToastFree(){}
 
@@ -26,22 +37,49 @@ public class ToastFree {
 
     // short to display the message
     public void showTextShort(@NonNull Context context, @NonNull String message){
-        if (textToast == null){
-            textToast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        if (toast_short == null){
+            toast_short = Toast.makeText(context, message, Toast.LENGTH_SHORT);
         }else {
-            textToast.setText(message);
+            toast_short.setText(message);
         }
-        textToast.show();
+        toast_short.show();
     }
 
     // long to display the message
     public void showTextLong(@NonNull Context context, @NonNull String message){
-        if (textToast == null){
-            textToast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        if (toast_long == null){
+            toast_long = Toast.makeText(context, message, Toast.LENGTH_LONG);
         }else {
-            textToast.setText(message);
+            toast_long.setText(message);
         }
-        textToast.show();
+        toast_long.show();
+    }
+
+    /**
+     *
+     * @param context
+     * @param drawableResId
+     * @param message
+     * @param duration #{DURATION_SHORT}、#{DURATION_LONG}
+     */
+    public void showImg(Context context, @DrawableRes int drawableResId, String message, int duration){
+        Toast toastImg = new Toast(context);
+        // inflate view
+        View view = LayoutInflater.from(context).inflate(R.layout.view_toast_img_short, null);
+        toastImg.setView(view);
+        toastImg.setGravity(Gravity.CENTER, 0, 0);
+        toastImg.setDuration(duration);
+        TextView tv_message = view.findViewById(R.id.tv_message);
+        if (tv_message != null){
+            if (TextUtils.isEmpty(message)){
+                tv_message.setVisibility(View.GONE);
+            }else {
+                tv_message.setVisibility(View.VISIBLE);
+                tv_message.setText(message);
+            }
+        }
+
+        toastImg.show();
     }
 
 
